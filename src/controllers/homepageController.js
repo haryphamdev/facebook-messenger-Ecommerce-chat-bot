bc832c93acd758c54eee09b3fed6dcc6f207a2a7
Hary Pham <haryphamdev@gmail.com>
2021-02-27 15:42:41 +0700

    update

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -40,6 +40,9 @@ let getWebhook = (req, res) => {
 
 let postWebhook = (req, res) => {
     let body = req.body;
+    console.log('---------------user')
+    console.log(body)
+    console.log('---------------user')
 
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
